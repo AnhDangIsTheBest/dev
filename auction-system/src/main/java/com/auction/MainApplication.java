e4bb7a658ceae4d65ed25324242fcf2a0f1351d5
Mingizme <daoducminhpc@gmail.com>
2026-04-22 14:04:27 +0700

    Update MainApplication.java

--- auction-system/src/main/java/com/auction/MainApplication.java
+++ auction-system/src/main/java/com/auction/MainApplication.java
@@ -1,4 +1,25 @@
 package com.auction;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainApplication {
+    private static Stage stg;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        stg = primaryStage;
+        primaryStage.setResizable(false);
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        primaryStage.setTitle("76 auction");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("/fxml/"+fxml));
+        stg.getScene().setRoot(pane);
+    }
 }
